8184da1f46320ed272aea2e4d0fe27237b1e94fd
Vsevolod Romashov <7@7vn.ru>
2014-10-11 19:22:39 +0400

    rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,22 @@
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    # be_bigger_than(2).and_smaller_than(4).description
+    #   # => "be bigger than 2 and smaller than 4"
+    # ...rather than:
+    #   # => "be bigger than 2"
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+  
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+  
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+  
+  config.disable_monkey_patching!
+  config.profile_examples = 10
+  
+  config.order = :random
+  Kernel.srand config.seed
+end
