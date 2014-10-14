ba99e797895679ec084a9af0620aa484ea241069
Vsevolod Romashov <7@7vn.ru>
2014-10-15 02:19:57 +0400

    Adapters::MemoryAdapter

---
+++ lib/yadm/adapters/memory_adapter.rb
@@ -0,0 +1,81 @@
+module YADM
+  module Adapters
+    class MemoryAdapter
+      attr_reader :collections
+      private :collections
+      
+      def initialize(connection_params = {})
+        # Memory adapter doesn't need any connection.
+        @collections = Hash.new do |hash, collection_name|
+          hash[collection_name] = Collection.new
+        end
+      end
+      
+      def get(collection_name, id)
+        collections[collection_name].get(id)
+      end
+      
+      def add(collection_name, object)
+        collections[collection_name].add(object)
+      end
+      
+      def change(collection_name, id, new_attributes)
+        collections[collection_name].change(id, new_attributes)
+      end
+      
+      def remove(collection_name, id)
+        collections[collection_name].remove(id)
+      end
+      
+      def count(collection_name)
+        collections[collection_name].count
+      end
+      
+      class Collection
+        attr_reader :objects
+        private :objects
+        
+        def initialize
+          @objects = {}
+        end
+        
+        def get(id)
+          objects.fetch(id)
+        end
+        
+        def add(object)
+          next_id.tap do |new_id|
+            objects[new_id] = object
+          end
+        end
+        
+        def change(id, new_attributes)
+          objects[id].update(new_attributes)
+        end
+        
+        def remove(id)
+          objects.delete(id)
+        end
+        
+        def count
+          objects.count
+        end
+        
+      private
+        def next_id
+          id_sequence.next
+        end
+        
+        def id_sequence
+          Enumerator.new do |yielder|
+            id = 0
+            loop do
+              id += 1
+              yielder.yield id
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/yadm/adapters/memory_adapter_spec.rb
@@ -0,0 +1,63 @@
+require 'yadm/adapters/memory_adapter'
+
+RSpec.describe YADM::Adapters::MemoryAdapter do
+  let(:person_attributes) do
+    { name: 'John', email: 'john@example.com' }
+  end
+  
+  describe '.get' do
+    before(:each) do
+      subject.add(:people, person_attributes)
+    end
+    
+    context 'with a valid id' do
+      it 'returns the record with the specified id' do
+        expect(subject.get(:people, 1)).to eq(person_attributes)
+      end
+    end
+    
+    context 'with an invalid id' do
+      it 'raises' do
+        expect {
+          subject.get(:people, 11)
+        }.to raise_error(KeyError)
+      end
+    end
+  end
+  
+  describe '.add' do
+    it 'adds a new record' do
+      expect {
+        subject.add(:people, person_attributes)
+      }.to change { subject.count(:people) }.by(1)
+    end
+    
+    it 'returns id of the created record' do
+      expect(subject.add(:people, person_attributes)).to eq(1)
+    end
+  end
+  
+  describe '.change' do
+    before(:each) do
+      subject.add(:people, person_attributes)
+    end
+    
+    it 'changes an existing record' do
+      expect {
+        subject.change(:people, 1, name: 'Jack')
+      }.to change { subject.get(:people, 1)[:name] }.to('Jack')
+    end
+  end
+  
+  describe '.remove' do
+    before(:each) do
+      subject.add(:people, person_attributes)
+    end
+    
+    it 'removes the record with the specified id' do
+      expect {
+        subject.remove(:people, 1)
+      }.to change { subject.count(:people) }.by(-1)
+    end
+  end
+end
