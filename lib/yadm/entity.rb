48042887930a119637cf79dfbbd571c048b42fda
Vsevolod Romashov <7@7vn.ru>
2014-10-20 02:45:27 +0400

    Entity#attributes returns a copy of the attributes hash

--- lib/yadm/entity.rb
+++ lib/yadm/entity.rb
@@ -1,13 +1,15 @@
 module YADM
   module Entity
     def initialize(new_attributes)
+      @attributes = {}
+      
       self.class.attributes.each do |attr_name|
-        attributes[attr_name] = fetch(new_attributes, attr_name)
+        @attributes[attr_name] = fetch(new_attributes, attr_name)
       end
     end
     
     def attributes
-      @attributes ||= {}
+      @attributes.dup
     end
     
   private
@@ -29,11 +31,11 @@ module YADM
         attributes.add(attr_name)
         
         define_method(attr_name) do
-          attributes[attr_name]
+          @attributes[attr_name]
         end
         
         define_method("#{attr_name}=") do |new_value|
-          attributes[attr_name] = new_value
+          @attributes[attr_name] = new_value
         end unless readonly
       end
       
