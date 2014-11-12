0baf6938d4191b6fb379da17e863e471098ae7a2
Vsevolod Romashov <7@7vn.ru>
2014-11-12 10:56:24 +0300

    enumerable queries

--- lib/yadm/query.rb
+++ lib/yadm/query.rb
@@ -2,6 +2,8 @@ module YADM
   class Query
     attr_reader :criteria, :arguments
     
+    include Enumerable
+    
     def initialize(criteria = Criteria.new, arguments = [])
       @criteria  = criteria
       @arguments = arguments
@@ -9,6 +11,18 @@ module YADM
     
     def merge(new_criteria, new_arguments)
       self.class.new(criteria.merge(new_criteria), arguments + new_arguments)
+    end
+    
+    def to_a
+      repository.send_query(self)
+    end
+    
+    def each(&block)
+      to_a.each(&block)
+    end
+    
+    def repository
+      raise NotImplementedError
     end
   end
 end
--- spec/yadm/query_spec.rb
+++ spec/yadm/query_spec.rb
@@ -13,4 +13,33 @@ RSpec.describe YADM::Query do
       expect(new_query.arguments).to eq([1])
     end
   end
+  
+  describe '#to_a' do
+    let(:repository) { double('Repository') }
+    let(:entity)     { double('Entity') }
+    
+    before(:each) do
+      allow(subject).to receive(:repository).and_return(repository)
+      allow(repository).to receive(:send_query).with(subject).and_return([entity])
+    end
+    
+    it 'executes the query and returns the results' do
+      expect(subject.to_a).to eq([entity])
+    end
+  end
+  
+  describe 'Enumerable methods' do
+    let(:first_entity)  { double('First entity') }
+    let(:second_entity) { double('Second entity') }
+    
+    before(:each) do
+      allow(subject).to receive(:to_a).and_return([first_entity, second_entity])
+    end
+    
+    it 'use #to_a' do
+      expect { |b| subject.each(&b) }.to yield_successive_args(first_entity, second_entity)
+      expect(subject.first).to eq(first_entity)
+      expect(subject.count).to eq(2)
+    end
+  end
 end
