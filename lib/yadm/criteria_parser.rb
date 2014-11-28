296043a0063d2d81f412a83c0b99a494600c841d
Vsevolod Romashov <7@7vn.ru>
2014-11-28 10:45:41 +0300

    arguments support in CriteriaParser

--- lib/yadm/criteria_parser.rb
+++ lib/yadm/criteria_parser.rb
@@ -8,8 +8,12 @@ module YADM
       @block = block
     end
     
-    def result
-      instance_eval(&block).result
+    def result(arguments_group)
+      arguments = block.arity.times.map do |index|
+        YADM::Criteria::Expression::Argument.new(arguments_group, index)
+      end
+      
+      instance_exec(*arguments, &block).result
     end
     
     %i(with ascending_by descending_by first).each do |method_name|
@@ -19,8 +23,8 @@ module YADM
     end
     
     class << self
-      def parse(block)
-        new(block).result
+      def parse(block, arguments_group)
+        new(block).result(arguments_group)
       end
     end
     
--- spec/yadm/criteria_parser_spec.rb
+++ spec/yadm/criteria_parser_spec.rb
@@ -26,31 +26,62 @@ def build_attribute(name)
   YADM::Criteria::Expression::Attribute.new(name)
 end
 
+def build_argument(group, index)
+  YADM::Criteria::Expression::Argument.new(group, index)
+end
+
 RSpec.describe YADM::CriteriaParser do
   describe '.parse' do
-    let(:block) do
-      proc do
-        with { age > 12 }.descending_by { age }.ascending_by { id }.first(5)
+    context 'without arguments' do
+      let(:block) do
+        proc do
+          with { age > 12 }.descending_by { age }.ascending_by { id }.first(5)
+        end
+      end
+      
+      let(:expected_criteria) do
+        build_criteria(
+          condition: build_condition(
+            build_expression(build_attribute(:age), :>, 12)
+          ),
+          order: build_order(
+            [
+              build_order_clause(:desc, build_attribute(:age)),
+              build_order_clause(:asc, build_attribute(:id))
+            ]
+          ),
+          limit: build_limit(5)
+        )
+      end
+      
+      it 'returns the parsed YADM::Criteria' do
+        expect(described_class.parse(block, :first)).to eq(expected_criteria)
       end
     end
     
-    let(:expected_criteria) do
-      build_criteria(
-        condition: build_condition(
-          build_expression(build_attribute(:age), :>, 12)
-        ),
-        order: build_order(
-          [
-            build_order_clause(:desc, build_attribute(:age)),
-            build_order_clause(:asc, build_attribute(:id))
-          ]
-        ),
-        limit: build_limit(5)
-      )
-    end
-    
-    it 'returns the parsed YADM::Criteria' do
-      expect(described_class.parse(block)).to eq(expected_criteria)
+    context 'with arguments' do
+      let(:block) do
+        proc do |min_age, limit|
+          with { age > min_age }.first(limit)
+        end
+      end
+      
+      let(:expected_criteria) do
+        build_criteria(
+          condition: build_condition(
+            build_expression(
+              build_attribute(:age),
+              :>,
+              build_argument(:first, 0)
+            )
+          ),
+          limit: build_limit(build_argument(:first, 1))
+        )
+      end
+      
+      it 'returns the parsed YADM::Criteria' do
+        expect(described_class.parse(block, :first)).to eq(expected_criteria)
+      end
     end
   end
 end
